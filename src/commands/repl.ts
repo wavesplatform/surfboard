3c21962a72a6a5c7ce705d12e03c139d4489b33d
Alexey Nagorny <anagorny@wavesplatform.com>
2019-08-08 13:39:26 +0300

    :large_blue_diamond: update version and fix bugs :bug:

--- src/commands/repl.ts
+++ src/commands/repl.ts
@@ -1,18 +1,49 @@
-import repl from 'repl';
+import repl, { REPLServer } from 'repl';
 import { repl as compiler, version } from '@waves/ride-js';
 import { Command } from '@oclif/command';
+
+let diamond: string;
+switch (process.platform) {
+    case 'darwin':
+    case 'linux':
+        diamond = '\x1b[34mRIDE \x1b[0m\uD83D\uDD37 \x1b[90m>\x1b[94m>\x1b[0m\x1b[34m>\x1b[0m ';
+        break;
+    case  'win32':
+        diamond = '\x1b[34mRIDE >\x1b[0m ';
+        break;
+    default:
+        diamond = '\x1b[34mRIDE >\x1b[0m ';
+        break;
+}
+
+function print(repl: REPLServer, str: string) {
+    console.log(str);
+    repl.displayPrompt();
+}
+
 
 export default class extends Command {
     async run() {
         process.stdout.write(`\x1b[1m${'Welcome to \x1b[34mRIDE\x1b[0m repl\nCompiler version ' + version}\x1b[0m\n\n`);
         repl.start({
-            prompt: '\x1b[34mRIDE \x1b[0m\uD83D\uDD37 \x1b[90m>\x1b[94m>\x1b[0m\x1b[34m>\x1b[0m ',
+            prompt: diamond,
             eval: function (input, context, filename, cb) {
                 const res = compiler(input);
-                cb('error' in res ? res.error : null, 'result' in res ? res.result : null);
+                if ('result' in res) {
+                    if (typeof res.result === 'string') {
+                        print(this, res.result);
+                    } else {
+                        cb(null, res.result);
+                    }
+                } else if ('error' in res) print(this, `\x1b[31m${res.error}\x1b[0m`);
             }
         });
     }
 }
+
+
+
+
+
 
 
