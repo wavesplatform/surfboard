0c306b41845da3c93007e978e326b4e2f3fb2ba9
Daniil <lobanovdanil97@gmail.com>
2021-02-24 11:15:46 +0300

    imports v1.0

--- src/commands/compile.ts
+++ src/commands/compile.ts
@@ -1,9 +1,9 @@
 import { Command } from '@oclif/command';
 import cli from 'cli-ux';
-import { compile, ICompilationResult } from '@waves/ride-js';
+import { compile, ICompilationResult, scriptInfo } from '@waves/ride-js';
 import * as flags from '@oclif/command/lib/flags';
 
-import { getFileContent } from '../utils';
+import { getFileContent, getLibContent } from '../utils';
 
 export default class Compile extends Command {
     static description = 'compile ride file';
@@ -22,14 +22,29 @@ export default class Compile extends Command {
         })
     };
 
-    private compile = async (file: string): Promise<ICompilationResult> => {
-        const resultOrError = compile(file, 3);
+    private compile = async (filePath: string): Promise<ICompilationResult> => {
+        let file = '';
+        try {
+            file = getFileContent(filePath);
+        } catch (error) {
+            this.error(error.message);
+        }
+
+        // if (file) {
+        const info = scriptInfo(file);
+        if ('error' in info) throw 'invalid scriptInfo';
+        const {imports} = info;
+        let libs = {} as Record<string, string>;
+        imports.forEach(path => libs[path] = getLibContent(filePath, path))
+
+        const resultOrError = compile(file, 3, libs);
 
         if ('error' in resultOrError) {
             throw new Error(resultOrError.error);
         }
 
         return resultOrError;
+        // }
     };
 
     async run() {
@@ -37,31 +52,23 @@ export default class Compile extends Command {
         const {flags} = this.parse(Compile);
 
         if (args.file) {
-            let file;
-            try {
-                file = getFileContent(args.file);
-            } catch (error) {
-                this.error(error.message);
-            }
-            if (file) {
-                this.compile(file)
-                    .then((result: ICompilationResult) => {
-                        if (flags.fullInfo) {
-                            cli.styledJSON({
-                                base64: result.result.base64,
-                                size: result.result.size,
-                                complexity: result.result.complexity
-                            });
-                        } else {
-                            cli.log(result.result.base64);
-                        }
-                    })
-                    .catch((error: Error) => {
-                        cli.styledHeader(`${args.file} was not compiled`);
+            this.compile(args.file)
+                .then((result: ICompilationResult) => {
+                    if (flags.fullInfo) {
+                        cli.styledJSON({
+                            base64: result.result.base64,
+                            size: result.result.size,
+                            complexity: result.result.complexity
+                        });
+                    } else {
+                        cli.log(result.result.base64);
+                    }
+                })
+                .catch((error: Error) => {
+                    cli.styledHeader(`${args.file} was not compiled`);
 
-                        console.error('Error message:', error.message);
-                    });
-            }
+                    console.error('Error message:', error.message);
+                });
         }
     }
 }
